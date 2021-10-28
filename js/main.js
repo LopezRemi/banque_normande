28ba3a3db3f2de9867cbc90bc3fdf11b88a4d6a1
Ryuu76000 <r.lopez76000@gmail.com>
2021-10-28 12:04:47 +0200

    layer done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,46 @@
-function negative(num){
-    return -Math.abs(num)
-}
+document.body.onload = addElement;
 
-negative(12);
-console.log(negative(0))
+
+function addElement() {
+    let mainLayerDiv = document.createElement('DIV')
+    mainLayerDiv.classList.add("mainLayer")
+    document.body.appendChild(mainLayerDiv);
+
+    let layerDiv = document.createElement('DIV')
+    layerDiv.classList.add("divLayer")
+    mainLayerDiv.appendChild(layerDiv)
+
+    let titleDiv = document.createElement('H1')
+    titleDiv.classList.add("titleLayer")
+    titleDiv.innerText = ('Règles du site')
+    layerDiv.appendChild(titleDiv)
+
+    let textLayer = document.createElement('DIV')
+    textLayer.classList.add("textLayer")
+    layerDiv.appendChild(textLayer)
+    let httpRequest = new XMLHttpRequest()
+ httpRequest.onreadystatechange = function() {
+     if (httpRequest.readyState === XMLHttpRequest.DONE) {
+         if (httpRequest.status === 200) {
+             let text = document.getElementsByClassName("textLayer");
+             text[0].innerText = httpRequest.responseText
+             console.log(httpRequest.responseText)
+         } 
+    }
+}    
+    httpRequest.open('GET', 'data/layer.txt');
+    httpRequest.send();
+
+    let layerBtn = document.createElement('button')
+    layerBtn.classList.add('btn','btn-primary','layerBtn')
+    layerBtn.innerHTML = "J'ai compris"
+    layerDiv.appendChild(layerBtn)
+
+    document.getElementsByClassName("layerBtn")
+    layerBtn.addEventListener("click",(e)=>{
+    mainLayerDiv.classList.add("none")
+
+    })
+
+};
+
